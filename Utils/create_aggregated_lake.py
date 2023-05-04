c6641b49bdd5e1974508e847ec8aef5ca570d128
FatemehAhmadi94 <fatemeh.ahmadi.compeng@gmail.com>
2023-05-04 11:38:43 +0200

    Added create_aggregated_lakes

---
+++ Utils/create_aggregated_lake.py
@@ -0,0 +1,56 @@
+import os 
+import pickle
+import shutil
+
+def create_aggregated_lakes(separated_sandbox_path, aggregated_dirty_sandbox_path, aggregated_clean_sandbox_path, dirty_file_name, clean_file_name, results_path):
+    """
+    This function creates the aggregated lake from the separated sandbox.
+    It moves the dirty_clean.csv and clean.csv files from each sandbox to the aggregated lakes.
+    It also creates a dictionary of the tables and their new names.
+
+    Parameters
+    ----------
+    separated_sandbox_path : str
+        The path to the separated sandbox.
+    aggregated_dirty_sandbox_path : str
+        The path to the aggregated dirty sandbox.
+    aggregated_clean_sandbox_path : str
+        The path to the aggregated clean sandbox.
+    dirty_file_name : str
+        The name of the dirty file.
+    clean_file_name : str
+        The name of the clean file.
+    """
+    count = 0
+    tables_dict = {}
+    for subdir, dirs, files in os.walk(separated_sandbox_path):
+        if subdir != separated_sandbox_path:
+            print(subdir)
+            new_table_name = '{}.csv'.format(count)
+            tables_dict[os.path.basename(subdir)] = new_table_name
+            # Move dirty_clean.csv to aggregated_dirty_sandbox_path
+            file = dirty_file_name
+            src = os.path.join(subdir, file)
+            dst = os.path.join(aggregated_dirty_sandbox_path, new_table_name)
+            shutil.copy(src, dst)
+
+            # Move clean.csv to aggregated_clean_sandbox_path
+            file = clean_file_name
+            src = os.path.join(subdir, file)
+            dst = os.path.join(aggregated_clean_sandbox_path, new_table_name)
+            shutil.copy(src, dst)
+
+            count += 1
+    print("table_dict", tables_dict)
+    with open(os.path.join(results_path, 'tables_dict.pickle'), 'wb') as handle:
+        pickle.dump(tables_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+separated_sandbox_path = "/home/fatemeh/EDS-BaseLines/Uni-Detect/datasets/data-gov/data-gov-sandbox"
+aggregated_dirty_sandbox_path = "/home/fatemeh/EDS-BaseLines/Uni-Detect/datasets/data-gov/aggregated_dirty"
+aggregated_clean_sandbox_path = "/home/fatemeh/EDS-BaseLines/Uni-Detect/datasets/data-gov/aggregated_clean"
+results_path = "/home/fatemeh/EDS-BaseLines/Uni-Detect/datasets/data-gov"
+dirty_file_name = "dirty_clean.csv"
+clean_file_name = "clean.csv"
+
+create_aggregated_lakes(separated_sandbox_path, aggregated_dirty_sandbox_path, aggregated_clean_sandbox_path, dirty_file_name, clean_file_name, results_path)
