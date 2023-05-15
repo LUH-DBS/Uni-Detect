5d950d8debd0123b39a6787168b1fe69b798ff0c
FatemehAhmadi94 <fatemeh.ahmadi.compeng@gmail.com>
2023-05-15 15:59:31 +0200

    Fixed tokenizing in get_prev_range

--- ud_utils.py
+++ ud_utils.py
@@ -82,7 +82,7 @@ def get_prev_range(tokens_dict: dict, col: pd.Series) -> float:
     prev_sum = -1
     col = col.astype(str)
 
-    tokens_list_col = [token for idx, value in col.items() for token in word_tokenize(value)]
+    tokens_list_col = [token for idx, value in col.items() for token in value.split()]
     tokens_set_col = set(tokens_list_col)
     prev_sum = sum(tokens_dict.get(token, 1) for token in tokens_set_col)
     prev_avg = prev_sum / len(tokens_set_col)
